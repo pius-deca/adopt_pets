e789a85bada13c9339c78d48e8a9bf67f6cd7759
PiusAremieye <ebi44ril@gmail.com>
2020-02-21 21:10:59 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+const Pet = ({name, animal, breed}) => {
+  return React.createElement("div", {}, [
+    React.createElement("h4", {}, name),
+    React.createElement("h4", {}, animal),
+    React.createElement("h4", {}, breed)
+  ])
+}
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name : "Zack",
+      animal : "Dog",
+      breed : "German Sheperd"
+    }),
+    React.createElement(Pet, {
+      name : "Spooky",
+      animal : "Bird",
+      breed : "Wood Picker"
+    })
+  ])
+}
+
+
+export default App;
